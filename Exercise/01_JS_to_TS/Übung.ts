76255c982b614fbb186a9d3982f7fd410815013d
rdnscr <raphael.ritter@bluewin.ch>
2023-06-15 15:55:14 +0200

    added proper tsconfig to run injection example

---
+++ Exercise/01_JS_to_TS/Übung.ts
@@ -0,0 +1,22 @@
+function configure(configuration) {
+    console.log('farbe: ' + configuration.colour);
+    
+    var i;
+    for (i = 0; index < configuration.routs.length; ++i) {
+        console.log('route: ' + configuartion.routes[i]);
+    }
+    return true;
+}
+
+var config = 
+{
+    color: 'red', 
+    routes: ['home', 'list']
+};
+
+
+var ret = configure(config);
+
+if(ret) {
+    console.log('successfull configured.');
+}
